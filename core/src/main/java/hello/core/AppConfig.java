1a08810e560391b3f22f958532e3ad0ef31ce742
kong0527 <o____ri@naver.com>
2022-04-25 22:36:10 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// Application 전체를 할당하고 구성
+// Application 전체를 할당하고 구성 (설정정보)
+@Configuration
 public class AppConfig {
 
+    @Bean // 메소드들이 스프링 컨테이너에 등록됨
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        // 할인정책을 바꾸고 싶을 땐 이 부분만 수정을 해주면 되는 것
+        return new RateDiscountPolicy();
     }
 
 
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     // psvm 치고 엔터치면 나옴,,,,,,대박
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); //MemberServiceImpl 이 들어가게 됨
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService(); //MemberServiceImpl 이 들어가게 됨
         //MemberService memberService = new MemberServiceImpl();
+
+        // Spring 사용 ver
+        // @Bean에 등록된 애들 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
